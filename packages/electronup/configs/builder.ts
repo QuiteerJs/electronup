76241fdd20cd6861d04a2affbb824135b13bcaa2
quiteer <p602985@yeah.net>
2024-01-30 01:48:21 +0800

    修改默认配置

--- packages/electronup/configs/builder.ts
+++ packages/electronup/configs/builder.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'path'
 import type { CliOptions } from 'electron-builder'
-// import { Arch, Platform } from 'electron-builder'
 import { readJSON } from 'fs-extra'
 import type { BuilderConfig, ElectronupConfig } from '../typings/electronup'
 import { DefaultDirs, store } from '../utils'
@@ -20,48 +19,36 @@ import { DefaultDirs, store } from '../utils'
 // const linuxTargetFormats = ['AppImage', 'snap', 'debian', 'rpm', 'freebsd', 'pacman', 'p5p', 'apk', ...allTargetFormats]
 // const linuxTargets = Platform.LINUX.createTarget(linuxTargetFormats, Arch.armv7l, Arch.arm64, Arch.x64)
 
-export async function getBuilderConfig(config: BuilderConfig, allConfig: ElectronupConfig) {
+export async function getBuilderConfig(config: BuilderConfig, allConfig: ElectronupConfig): Promise<CliOptions> {
   const packages = await readJSON(resolve(store.root, 'package.json'))
 
-  const outConfig = {
-    targets: store.targets,
-    x64: store.x64,
-    ia32: store.ia32,
-    armv7l: store.armv7l,
-    arm64: store.arm64,
-    universal: store.universal,
-    dir: store.dir
+  const defaultConfig: CliOptions['config'] = {
+    asar: store.asar,
+    appId: 'org.quiteer.electronup',
+    productName: packages.name,
+    protocols: {
+      name: packages.name,
+      schemes: ['deeplink']
+    },
+    nsis: {
+      oneClick: false,
+      language: '2052',
+      perMachine: true,
+      allowElevation: true,
+      allowToChangeInstallationDirectory: true,
+      runAfterFinish: true,
+      createDesktopShortcut: true,
+      createStartMenuShortcut: true,
+      artifactName: `${packages.name} \${arch} Setup ${packages.version}.\${ext}`
+    },
+    files: [`${allConfig.resourceDir || DefaultDirs.resourceDir}/**/*`],
+    extraFiles: [allConfig.libDir || DefaultDirs.libDir],
+    directories: {
+      output: allConfig.outDir || config.directories?.output || DefaultDirs.outDir
+    },
+    ...config
   }
 
-  const defaultConfig: CliOptions = {
-    config: {
-      asar: store.asar,
-      appId: 'org.quiteer.electronup',
-      productName: packages.name,
-      protocols: {
-        name: packages.name,
-        schemes: ['deeplink']
-      },
-      nsis: {
-        oneClick: false,
-        language: '2052',
-        perMachine: true,
-        allowElevation: true,
-        allowToChangeInstallationDirectory: true,
-        runAfterFinish: true,
-        createDesktopShortcut: true,
-        createStartMenuShortcut: true,
-        artifactName: `${packages.name} \${arch} Setup ${packages.version}.\${ext}`
-      },
-      files: [`${allConfig.resourceDir || DefaultDirs.resourceDir}/**/*`],
-      extraFiles: [allConfig.libDir || DefaultDirs.libDir],
-      directories: {
-        output: allConfig.outDir || config.directories?.output || DefaultDirs.outDir
-      },
-      ...config
-    }
-  }
-
-  return { ...defaultConfig, ...outConfig }
+  return { targets: store.targets, config: defaultConfig }
 }
 
